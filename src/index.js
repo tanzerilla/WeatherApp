dafee7b0e23e52a71c7fc0955c36d5df5d98ed6a
dgoncalves <diogo.goncalves@milenar.pt>
2019-08-14 16:18:48 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App_1.css';
+
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
